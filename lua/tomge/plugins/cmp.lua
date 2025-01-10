62aa8513ca9e12b7cd7b3cc85424d4a075b5a4f7
Tom Erik Günther <tomguenther@proton.me>
2025-01-10 13:04:39 +0100

    added english language completion for markdown files

--- lua/tomge/plugins/cmp.lua
+++ lua/tomge/plugins/cmp.lua
@@ -4,40 +4,59 @@ local luasnip = require("luasnip")
 luasnip.config.setup({})
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  completion = { completeopt = "menu,menuone,noinsert" },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<Right>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete({}),
-    ["<C-l>"] = cmp.mapping(function()
-      if luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      end
-    end, { "i", "s" }),
-    ["<C-h>"] = cmp.mapping(function()
-      if luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      end
-    end, { "i", "s" }),
-  }),
-  sources = {
-    { name = "obsidian" },
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	completion = { completeopt = "menu,menuone,noinsert" },
+	mapping = cmp.mapping.preset.insert({
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<Right>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete({}),
+		["<C-l>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { "i", "s" }),
+		["<C-h>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
+	}),
+	sources = {
+		{ name = "obsidian" },
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "path" },
+	},
 })
 
 cmp.setup.filetype("markdown", {
-  sources = cmp.config.sources({
-    { name = "buffer" },
-  }),
+	sources = cmp.config.sources({
+		{ name = "buffer" },
+		{
+			name = "look",
+			keyword_length = 2,
+			option = {
+				convert_case = true,
+				loud = true,
+				--dict = '/usr/share/dict/words'
+			},
+		},
+		{
+			name = "spell",
+			option = {
+				keep_all_entries = false,
+				enable_in_context = function()
+					return true
+				end,
+				preselect_correct_word = true,
+			},
+		},
+	}),
 })
